612ab962951102290ba278e37b3ed3c455a81895
Alex Gaetano Padula <me@alexpadula.com>
2024-01-13 15:01:55 -0500

    Remove QUEUES command, useless

--- main.go
+++ main.go
@@ -226,6 +226,10 @@ func (fairyMQ *FairyMQ) StartUDPListener() {
 					}
 
 					switch {
+					//case bytes.HasPrefix(plaintext, []byte("QUEUES")):
+					//	queues := maps.Keys(fairyMQ.Queues)
+					//	fairyMQ.Conn.WriteToUDP(append([]byte(strings.Join(queues, ",")), []byte("\r\n")...), addr)
+					//	goto cont
 					case bytes.HasPrefix(plaintext, []byte("FIRST IN ")):
 						spl := bytes.Split(plaintext, []byte("\r\n"))
 						queue := bytes.ReplaceAll(spl[0], []byte("FIRST IN "), []byte(""))
